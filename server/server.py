d98bedf51d8300dd0e631c17ee9914875f8e384a
MaxTheHuman <32000048+MaxTheHuman@users.noreply.github.com>
2020-03-06 13:11:42 +0300

    Add documentation

--- server/server.py
+++ server/server.py
@@ -12,6 +12,13 @@ next_id = 1
 
 class OnlineStore(Resource):
 
+    '''
+    Function get() is used to view items.
+    If no body is passed with the request, respond will be an array of all items stored in shop.
+    If json of format {"id": item_id} is passed, respond will contain only information about item
+    with given id. At this stage there are no checks that id is valid, i.e. there is an item with
+    given id.
+    '''
     def get(self):
         view_items_info = request.get_json()
         if view_items_info is None:
@@ -20,6 +27,13 @@ class OnlineStore(Resource):
             view_item_id = view_items_info["id"]
             return jsonify({"id": view_item_id, "info": items[view_item_id]})
 
+        
+    '''
+    Function post() is used to add items.
+    Body of this requests must be of given format:
+    {"name": item_name, "category": item_category}
+    Result of the function is item added to the store.
+    '''
     def post(self):
         global next_id
 
@@ -33,6 +47,13 @@ class OnlineStore(Resource):
 
         return {'item added': {"id": next_id - 1, "info": new_item}}, 201
 
+    '''
+    Function put() is used to update items.
+    Body of this requests must be of given format:
+    {"id": id_of_item_to_update, "name": item_name, "category": item_category}
+    Result of the function is item updated with new given values if it existed
+    before call, otherwise item with given id and values will be added.
+    '''
     def put(self):
         update_item_info = request.get_json()
         update_item_id = update_item_info["id"]
@@ -44,6 +65,12 @@ class OnlineStore(Resource):
 
         return {'item updated': {"id": update_item_id, "info": update_item}}, 202
 
+    '''
+    Function delete() is used to delete items.
+    Body of this requests must contain json of format {"id": id_of_item_to_delete}
+    Result of the function is item deleted from the store.
+    At this stage there are no checks that item with given id exists.
+    '''
     def delete(self):
         delete_item_info = request.get_json()
         delete_item_id = delete_item_info["id"]
